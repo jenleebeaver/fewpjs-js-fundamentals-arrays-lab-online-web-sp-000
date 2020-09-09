684d2d38fdda4305f0f63302cf4b7d31b2d13a03
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 05:40:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const append = pets
 append.push('Odie')
 console.log(append)
 //push method literally means to "append" or add to the end of the array 
+
+//2
+const prepend = pets 
+prepend.unshift('Odie')
